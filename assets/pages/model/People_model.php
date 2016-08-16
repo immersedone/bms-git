44286e1e0eb7ef4678e70b012d8ae14c2656f4e1
s3485754 <s3485754@student.rmit.edu.au>
2016-08-16 12:49:42 +1000

    Added model pages ready to construct functions
    Model pages are named and functions are grouped to match the schema
    tables

---
+++ assets/pages/model/People_model.php
@@ -0,0 +1,64 @@
+<?php
+class People_model extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    //List all people
+    function list_all_people()
+    {
+
+    }
+
+    /*
+    function list_all_employees()
+    {
+
+    }
+    */
+
+    /*
+    function list_all_volunteers()
+    {
+
+    }
+    */
+    //Add people to a project
+    function add_people_to_proj()
+    {
+
+    }
+
+    //Remove people from project
+    function remove_people_from_proj()
+    {
+
+    }
+
+    //View expenditures for a person
+    function view_person_expenditures()
+    {
+
+    }
+
+    //View reimbursements for a person
+    function view_person_reimbursements()
+    {
+
+    }
+
+    //Add an expenditure for a person
+    function add_person_expenditure()
+    {
+
+    }
+
+    //Remove epxenditure from a person
+    function remove_person_expenditure()
+    {
+
+    }
+}
+?>
