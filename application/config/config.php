d0c63b667d308b9f6232e0518a359923903df882
Truc Minh Phan <me@trucphan.com>
2016-08-21 16:56:58 +1000

    Change Base URL to Live Server

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://bms.dev/';
+$config['base_url'] = 'http://bms.auweb01.bluether.com.au/';
 
 /*
 |--------------------------------------------------------------------------
