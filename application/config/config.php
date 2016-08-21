cee80ae18486e53dd1fd80ea1b96f4d67bee3def
Truc Minh Phan <me@trucphan.com>
2016-08-21 21:13:01 +1000

    Update Routes

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://bms.auweb01.bluether.com.au/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
