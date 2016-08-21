b580f22db29ae5ee716070e5526a70977c59e9a5
s3485754 <s3485754@student.rmit.edu.au>
2016-08-21 16:19:46 +1000

    More model work
    Going out, so this is a checkpoint

--- application/models/People_model.php
+++ application/models/People_model.php
@@ -5,11 +5,28 @@ class People_model extends Grocery_crud_model {
         parent::construct();
     }
 
-    function list_all_people()
+    function view_all_people()
     {
         $this->db->select('*');
         $this->db->from('People');
           
+    }
+    
+        
+    //View expenditures for a person
+    function view_person_expenditures()
+    {
+        $this->db->select('*');
+        $this->db->from('Expenditure');
+        $this->db->where('SpentBy'=$chosen_person);
+    }
+
+    //View reimbursements for a person
+    function view_person_reimbursements()
+    {
+        $this->db->select('*');
+        $this->db->from('Reimbursement');
+        $this->db->where('PerID'=$chosen_person); 
     }
     /*
     function list_all_employees()
@@ -22,37 +39,28 @@ class People_model extends Grocery_crud_model {
 
     }
     */
+   
     
     //Add people to a project
     public function add_people_to_proj($data)
     {  
         $this->db->insert('PersonProject', $data);
     }
+    
+    
+    //Add an expenditure for a person
+    function add_person_expenditure()
+    {
+        $this->db->insert('PersonProject', $data);
+    }
+
 
     //Remove people from project
     function remove_people_from_proj()
     {
         
     }
-
-    //View expenditures for a person
-    function view_person_expenditures()
-    {
-
-    }
-
-    //View reimbursements for a person
-    function view_person_reimbursements()
-    {
-
-    }
-
-    //Add an expenditure for a person
-    function add_person_expenditure()
-    {
-
-    }
-
+    
     //Remove epxenditure from a person
     function remove_person_expenditure()
     {
