c185e8a7074b1cb09e9b2767eec5248a15eda4d6
Adam Salek <s3462148@student.rmit.edu.au>
2016-08-22 18:26:01 +1000

    Fixing funding controller

--- application/controllers/Funding.php
+++ application/controllers/Funding.php
@@ -25,10 +25,20 @@ class Funding extends CI_Controller {
 	public function funding() {
 
 		$crud = new grocery_CRUD();
-		$crud->set_theme('flexigrid');
+		$crud->set_model('Funding_GC');
 		$crud->set_table('Funding');
 		$crud->set_subject('Funding');
-		
+		$crud->basic_model->set_query_str('SELECT Proj.Name as ProjName, FB.BodyName as FBName, `Funding`.* from `Funding`
+		LEFT OUTER JOIN `FundingBody` FB on FB.FundBodyID=`Funding`.FundBodyID
+		LEFT OUTER JOIN `Project` Proj on Proj.ProjID=`Funding`.ProjID
+		ORDER BY Proj.Name
+		');
+		$crud->columns('ProjName', 'FBName', 'Amount', 'PaymentType', 'ApprovedBy', 'ApprovedOn');
+		$crud->set_display('ProjName', 'Project');
+		$crud->set_display('FBName', 'Funding Body');
+		$crud->set_display('PaymentType', 'Payment Type');
+		$crud->set_display('ApprovedBy', 'Approved By');
+		$crud->set_display('ApprovedOn', 'Approved On');
 
 		$output = $crud->render();
 
---
+++ application/models/Funding_GC.php
@@ -0,0 +1,64 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+    class Funding_GC extends grocery_CRUD_model {
+     
+        private  $query_str = ''; 
+        function __construct() {
+            parent::__construct();
+        }
+     
+        function get_list() {
+            $query=$this->db->query($this->query_str);
+     
+            $results_array=$query->result();
+            return $results_array;      
+        }
+     
+        public function set_query_str($query_str) {
+            $this->query_str = $query_str;
+        }
+    
+        function get_total_results() {
+            return count($this->get_list());
+        }
+
+        function delete_pp($userid, $projectid)
+        {
+
+            $resp = array();
+
+            if($this->db->simple_query("DELETE FROM PersonProject WHERE ProjID='$projectid' AND PerID='$userid' AND Role='EMPLOYEE' LIMIT 1")) {
+                $resp['success'] = TRUE;
+                $resp['success_message'] = "Successfully removed Person from Project.";
+            } else {
+                $resp['success'] = FALSE;
+                $resp['error_message'] = "Failed to remove Person from Project.\n Try again later or contact system administrator.";
+            }
+
+            echo json_encode($resp);
+        }
+
+        function insert_pp($personID, $projectID, $role) {
+
+            $resp = array();
+
+            if($this->db->simple_query("INSERT INTO PersonProject (ProjID, PerID, Role) VALUES('$projectID', '$personID', '$role')")) {
+                $resp['success'] = TRUE;
+                $resp['success_list_url'] = base_url() . "user/employee";
+                $resp['success_message'] = "Successfully added Person to Project.";
+            } else {
+                $resp['success'] = FALSE;
+                $resp['error_message'] = "Successfully added Person to Project.";
+                $resp['error_fields'] = "";
+            }
+
+            echo json_encode($resp);
+
+        }
+
+        function return_query($query_string) {
+            $query=$this->db->query($query_string);
+            $results_array=$query->result();
+            return $results_array;
+        }
+}
