b465c0b4d4b9aaa3b84e6edd29175fa39ea2bb88
Adam Salek <s3462148@student.rmit.edu.au>
2016-08-22 15:44:24 +1000

    Forgot comma

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -54,7 +54,7 @@ class Employee extends CI_Controller {
 		$crud->set_model('Employee_GC');
 		$crud->set_table('Person');
 		$crud->set_subject('Employee');
-		$crud->basic_model->set_query_str('SELECT Proj.Name, Proj.ProjID, `PersonProject`.Role as ProjRole, CONCAT(FirstName, " ", MiddleName, " ", LastName) as FullName, Sub.SuburbName as SubName`Person`.* FROM `Person` LEFT OUTER JOIN `PersonProject` ON `Person`.PerID=`PersonProject`.PerID LEFT OUTER JOIN `Project` Proj ON `PersonProject`.ProjID=Proj.ProjID WHERE `PersonProject`.Role="Employee" 
+		$crud->basic_model->set_query_str('SELECT Proj.Name, Proj.ProjID, `PersonProject`.Role as ProjRole, CONCAT(FirstName, " ", MiddleName, " ", LastName) as FullName, Sub.SuburbName as SubName, `Person`.* FROM `Person` LEFT OUTER JOIN `PersonProject` ON `Person`.PerID=`PersonProject`.PerID LEFT OUTER JOIN `Project` Proj ON `PersonProject`.ProjID=Proj.ProjID WHERE `PersonProject`.Role="Employee" 
 		LEFT OUTER JOIN `Suburb` Sub on Sub.Postcode = `Person`.SuburbID ORDER BY Proj.Name ASC');
 		$crud->columns("Name", "FullName", "Address", "SuburbID", "SubName", "WorkEmail", "PersonalEmail", "Mobile", "HomePhone");
 		$crud->display_as("Name", "Project Name");
