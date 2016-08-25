b171a2d4f060e26e67e463c758417a8f1a53b619
Adam Salek <s3462148@student.rmit.edu.au>
2016-08-26 05:57:29 +1000

    changing quotes

--- application/controllers/Reimbursements.php
+++ application/controllers/Reimbursements.php
@@ -26,7 +26,7 @@ class Reimbursements extends CI_Controller {
 		$crud = new grocery_CRUD();
 		$crud->set_theme('flexigrid');
 		$crud->set_model('Reimbursement_GC');
-		$crud->basic_model->set_query_str("SELECT CONCAT(P.FirstName, " ", P.MiddleName, " ", P.LastName) as FullName, R.* FROM Reimbursement R
+		$crud->basic_model->set_query_str("SELECT CONCAT(P.FirstName, ' ', P.MiddleName, ' ', P.LastName) as FullName, R.* FROM Reimbursement R
 		LEFT OUTER JOIN Person P on R.PerID=P.PerID");
 		$crud->set_table('Reimbursement');
 		$crud->set_subject('Reimbursement');
