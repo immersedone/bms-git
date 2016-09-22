220b5ba71554696ee3d6875a5a279f8f67a2df34
rbargholz <rbargholz@gmail.com>
2016-09-22 15:39:19 +1000

    Add Supervisor dropdown framework and patch add on Employee

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -163,11 +163,19 @@ class Employee extends CI_Controller {
         }
 		$crud->field_type("DaysWork", "multiselect", $daysArr);
 
+        //Supervisors
+        $supervisors = $crud->basic_model->return_query("SELECT PerID, FullName FROM Employee");
+        $visorArr = array();
+        foreach($supervisors as $spv){
+            $visorArr += [$spv->PerID=> $spv->FullName];
+        }
+        $crud->field_type("Supervisor", "dropdown", $visorArr);
+
         //Superannuation Funds
         $funds = $crud->basic_model->return_query("SELECT OptID, data FROM OptionType WHERE type='Fund'");
         $fundArr = array();
         foreach($funds as $fnd) {
-            $fundArr+= [$fnd->OptID=> $fnd->data];
+            $fundArr += [$fnd->OptID=> $fnd->data];
         }
         $crud->field_type("SuperFund", "dropdown", $fundArr);
 
