ed3502025aa573bdf4f28e18932086e3a5975b60
Truc Minh Phan <me@trucphan.com>
2016-11-08 15:06:40 +1100

    Re-work GenReport Forms & Add in Expenditure by Proj & Date Report

--- application/models/Genreport_model.php
+++ application/models/Genreport_model.php
@@ -36,13 +36,49 @@ class Genreport_model extends CI_Model {
 
     public function getReimbursements()
     {
-        $query = $this->db->query("SELECT Reimbursement.ReimID FROM Reimbursement LEFT OUTER JOIN Person ON Reimbursement.ApprovedBy=Person.PerID LEFT OUTER JOIN Person as Per ON Reimbursement.PerID=Per.PerID");
+        $query = $this->db->query("SELECT Reimbursement.ReimID, CONCAT(Per.FirstName, ' ', Per.MiddleName, ' ', Per.LastName) as FullName, Reimbursement.ExpList as ExpList FROM Reimbursement LEFT OUTER JOIN Person ON Reimbursement.ApprovedBy=Person.PerID LEFT OUTER JOIN Person as Per ON Reimbursement.PerID=Per.PerID");
         $reimb = array();
         foreach($query->result_array() as $row) {
-            $reimb[$row["ReimID"]] = $row["ReimID"];
+
+            if($row["ExpList"] !== "") {
+                $list = explode(',', $row["ExpList"]);
+
+                $amt = 0;
+
+                foreach($list as $l) {
+                    $qL = $this->db->query("SELECT Amount FROM Expenditure WHERE ExpID='" . $l . "' LIMIT 1");
+
+                    $r = $qL->row();
+                    $amt +=  $r->Amount;
+                }
+            } else {
+                $amt = 0;
+            }
+
+            $reimb[$row["ReimID"]] = '(' . $row["ReimID"] . ')  ' . $row["FullName"] . ' - $' . $amt;
         }
 
         return $reimb;
+    }
+
+    public function getSupervisors()
+    {
+        $query = $this->db->query("SELECT 
+            Per.PerID as PerID, 
+            CONCAT(Per.FirstName, ' ', Per.MiddleName, ' ', Per.LastName) as FullName,
+            Proj.Name as ProjName 
+            FROM Person Per 
+            LEFT OUTER JOIN PersonProject PP ON Per.PerID=PP.PerID 
+            LEFT OUTER JOIN OptionType Opt ON PP.Role=Opt.OptID 
+            LEFT OUTER JOIN Project Proj ON PP.ProjID=Proj.ProjID
+            WHERE Opt.type='Role' AND Opt.data='Supervisor'");
+        $supervisors = array();
+
+        foreach($query->result_array() as $row) {
+            $supervisors[$row["PerID"]] = $row["FullName"] . ' [' . $row["ProjName"] . ']';
+        }
+
+        return $supervisors;
     }
 
 }
--- application/views/genreport.php
+++ application/views/genreport.php
@@ -10,6 +10,8 @@
     <title>Generate a Report | Banksia Gardens</title>
 
 
+    <!-- jQuery UI -->
+    <link href="http://code.jquery.com/ui/1.12.0/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
     <!-- Bootstrap -->
     <link href="/assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
@@ -78,40 +80,86 @@
           <div class="row mTop">
             <div class="col-md-12 col-sm-12 col-xs-12 bg_norm">  
                 <div class="dashboard_generate"> 
-                    
+                    <div class="row x_custom x_title">
+                        <div class="col-md-12">
+                            <h3>Form</h3>
+                        </div>
+                    </div>
+
+
                     <div class="row x_custom">
                         <div class="col-md-12">
-                            <form onsubmit="validate()" method="POST" action="/user/genreport/createreport">
+                            <form onsubmit="validate()" class="genRep" method="POST" action="/user/genreport/createreport">
                                 
+                                <!-- First Row in Form -->
                                 <div class="col-md-12 control-wrap">
 
                                     <!-- Select Type of Report -->
                                     <div class="col-md-6">
-                                    <div class="control-form reportType">
-                                        <h4>Select Report Type</h4>
-                                        <select name="reportType">
-                                            <option value="0"> --- Please select a Report Type --- </option>
-                                            <!--<option value="allemp">All Employees</option>
-                                            <option value="allvol">All Volunteers</option>
-                                            <option value="allprj">All Projects</option>
-                                            <option value="allfun">All Funding</option>
-                                            <option value="prjovr">Project Overview</option>-->
-                                            <option value="empprj">Employees in a Project</option>
-                                            <option value="reimb">Reimbursements</option>
-                                            <!--<option value="volprj">Volunteers in a Project</option>
-                                            <option value="expprj">Expenditures in a Project</option>
-                                            <option value="reiprj">Reimbursements in a Project</option>
-                                            <option value="mstprj">Milestones in a Project</option>
-                                            <option value="funprj">Funding in a Project</option>-->
-                                        </select>
+	                                    <div class="control-form reportType">
+	                                        <h4>Report Type:</h4>
+	                                        <hr/>
+	                                        <select name="reportType" id="reportType" class="form-control">
+	                                            <option value="0"> --- Please select a Report Type --- </option>
+	                                            <option value="empprj">Employees in a Project</option>
+	                                            <option value="reimb">Reimbursement Report</option>
+	                                            <option value="exp_prj">Expenditure by Project and Date</option>
+	                                            <option value="exp_spv">Expenditure by Supervisor and Date</option>
+	                                            <option value="pend_mst">Pending Milestones</option>
+	                                            <option value="cdet_emp">Contact Details - All Employees</option>
+	                                            <option value="cdet_vol">Contact Details - All Volunteers</option>
+	                                            <option value="cont_srv">Contract and WWC - Status Review</option>
+	                                        </select>
+	                                    </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                    	<div class="control-form optionCoverPage">
+                                    		<h4>Cover Page (optional):</h4>
+                                    		<hr/>
+                                    		<div class="form-group">
+		                                        <label for="optionalCoverPage" class="control-label">Remove Cover Page for Report?</label>
+		                                        <input type="radio" value="YES" name="optionalCoverPage" required="required" checked="checked"/> Yes
+		                                        <input type="radio" value="NO" name="optionalCoverPage" required="required"/> No
+	                                        </div>
+	                                        <div class="form-group" id="customTitleCheck" style="display:none;">
+		                                        <label for="customTitleCheck" class="control-label">Add Custom Title to Cover Page? </label>
+		                                        <input type="radio" value="YES" name="customTitleCheck" required="required" checked="checked"/> Yes
+		                                        <input type="radio" value="NO" name="customTitleCheck" required="required"/> No
+	                                        </div>
+	                                    </div>
+                                    </div>
+                                </div>
+
+
+                                <!-- Custom Report Title -->
+                                <div class="col-md-12 control-wrap" id="customTitle" style="display:none;">
+
+                                    <!-- File Names -->
+                                    <div class="col-md-12">
+                                    <div class="control-form reportName">
+                                    <h4>Custom Report Title</h4>
+                                    <label for="title_one" class="control-label">Line 1:</label>
+                                    <input type="text" name="title_one" placeholder="Title Line 1" class="form-control"/>
+                                    <br/>
+                                    <label for="title_two" class="control-label">Line 2:</label>
+                                    <input type="text" name="title_two" placeholder="Title Line 2" class="form-control"/>
                                     </div>
                                     </div>
 
+                                </div>
+
+
+
+                                <!-- List Projects Form -->
+                                <div class="col-md-12 control-wrap hideForm" id="listProj"  style="display: none">
+
                                     <!-- Select Type of Report -->
-                                    <div class="col-md-6">
+                                    <div class="col-md-12">
                                     <div class="control-form listProjects">
                                         <h4>Select Project</h4>
-                                        <select name="project">
+                                        <hr/>
+                                        <select name="project" class="form-control">
                                             <option value="0"> --- Please select a project --- </option>
                                             <?php foreach($Projects as $key => $value) {
                                                 echo '<option value="' . $key . '">' . $value . '</option>';
@@ -124,12 +172,17 @@
                                     </div>
                                     </div>
 
-                                    <!-- Select Type of Report -->
-                                    <div class="col-md-6">
+                                </div>
+
+                                <!-- Reimbursements -->
+                                <div class="col-md-12 control-wrap hideForm" id="listReimb" style="display: none">
+
+                                    <div class="col-md-12">
                                     <div class="control-form listReimbursements">
                                         <h4>Select Reimbursement</h4>
-                                        <select name="reimbursement">
-                                            <option value="0"> --- Please select a project --- </option>
+                                        <hr/>
+                                        <select name="reimbursement" class="form-control">
+                                            <option value="0"> --- Please select a reimbursement --- </option>
                                             <?php foreach($Reimbursements as $key => $value) {
                                                 echo '<option value="' . $key . '">' . $value . '</option>';
                                             }
@@ -139,32 +192,54 @@
                                             
                                         </select>
                                     </div>
-                                    <div class="control-form optionCoverPage">
-                                        <label for="optionalCoverPage">Remove Cover Page for Reimbursement Report?</label>
-                                        <input type="checkbox" value="false" name="optionalCoverPage" />
+                                    
                                     </div>
+
+                                </div>
+
+                                <!-- Supervisors -->
+                                <div class="col-md-12 control-wrap hideForm" id="listSupervisor" style="display: none">
+
+                                    <div class="col-md-12">
+                                    <div class="control-form listSupervisors">
+                                        <h4>Select Supervisor</h4>
+                                        <hr/>
+                                        <select name="supervisor" class="form-control">
+                                            <option value="0"> --- Please select a supervisor --- </option>
+                                            <?php foreach($Supervisors as $key => $value) {
+                                                echo '<option value="' . $key . '">' . $value . '</option>';
+                                            }
+
+                                            ?>
+
+                                            
+                                        </select>
+                                    </div>
+                                    
                                     </div>
 
                                 </div>
 
                                
-                                <div class="col-md-12 control-wrap">
+                               	<!-- Date Range --> 
+                               	<div class="col-md-12 control-wrap hideForm" id="dateRange" style="display:none;">
+                               		<div class="col-md-12">
+                               		<div class="control-form">
+                               			<h4>Select Date Range</h4>
+                               			<hr/>
+                               			<label for="fromDate" class="control-label">From:</label>
+                               			<input type="date" name="fromDate" class="input-date form-control datepicker" placeholder="DD/MM/YYYY" required="required" />
+                               			<label for="toDate" class="control-label">To:</label>
+                               			<input type="date" name="toDate" class="input-date form-control datepicker" placeholder="DD/MM/YYYY" required="required" />
+                               		</div>
+                               		</div>
+                               	</div>
 
-                                    <!-- File Names -->
-                                    <div class="col-md-6">
-                                    <div class="control-form reportName">
-                                    <h4>Report Name</h4>
-                                    <label for="title_one">Line 1:</label>
-                                    <input type="text" name="title_one" placeholder="Title Line 1"/>
-                                    <br/>
-                                    <label for="title_two">Line 2:</label>
-                                    <input type="text" name="title_two" placeholder="Title Line 2"/>
-                                    </div>
-                                    </div>
 
-                                </div>
 
-                                <div class="col-md-12 control-wrap">
+                               	
+                                <!-- Form Submit Button-->
+                                <div class="col-md-12 control-wrap hideForm formSubmitCont"  style="display: none">
                                     <div class="col-md-12">
                                         <input type="submit" class="btn" value="Generate Report" style="float:right"/>
                                     </div>
@@ -186,6 +261,7 @@
 
     <!-- jQuery -->
     <script src="/assets/vendors/jquery/dist/jquery.min.js"></script>
+    <script   src="https://code.jquery.com/ui/1.12.0/jquery-ui.min.js"   integrity="sha256-eGE6blurk5sHj+rmkfsGYeKyZx3M4bG+ZlFyA7Kns7E="   crossorigin="anonymous"></script>
     <!-- Bootstrap -->
     <script src="/assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- FastClick -->
@@ -225,6 +301,132 @@
     <!-- Custom Theme Scripts -->
     <script src="/assets/js/custom.min.js"></script>
 
-   
+
+    <!-- jQuery UI-daterangepicker -->
+    <script>
+      $(document).ready(function() {
+
+      		var dateInput = $('.datepicker');
+      		dateInput.datepicker({
+      			yearRange: "-100:+0",
+      			dateFormat: "yy-mm-dd",
+      			changeMonth: true,
+      			changeYear: true
+      		});
+        });
+    </script>
+
+
+    <!-- Javascript for Forms -->
+   	<script type="text/javascript">
+
+   		
+
+
+   		//Associative Array of Containers + Inputs
+   		var indexInput = {
+   			listProj:"#listProj", 
+   			listReimb:"#listReimb",
+   			listSupervisor:"#listSupervisor",
+   			showDate:"#dateRange"
+		};
+
+   		//Individual Arrays for required elements
+   		var empprjArray = ["listProj"];
+   		var reimbArray = ["listReimb"];
+   		var exp_prjArray = ["listProj", "showDate"];
+   		var exp_spvArray = ["listSupervisor", "showDate"];
+   		var pend_mstArray = ["showDate"];
+   		var cdet_empArray = [];
+   		var cdet_volArray = [];
+   		var cont_srvArray = [];
+
+   		//Bind Report Types to an Array
+   		var allArray = {
+   			empprj:empprjArray, 
+   			reimb:reimbArray, 
+   			exp_prj:exp_prjArray, 
+   			exp_spv:exp_spvArray, 
+   			pend_mst:pend_mstArray, 
+   			cdet_emp:cdet_empArray, 
+   			cdet_vol:cdet_volArray, 
+   			cont_srv:cont_srvArray
+   		};
+
+
+   		//Show/Hide Custom Title
+   		//Also disable form inputs when not checked
+   		$('input[type=radio][name=optionalCoverPage]').change(function() {
+   			if(this.value == 'YES') {
+   				$('#customTitleCheck').slideUp();
+   				$('#customTitle input').prop("disabled", true);
+   			} else {
+   				$('#customTitleCheck').slideDown();
+   				$('#customTitle').slideDown();
+   			}
+   		});
+
+   		//Show/Hide Custom Title
+   		//Also disable form inputs when not checked
+   		$('input[type=radio][name=customTitleCheck]').change(function() {
+   			if(this.value == 'YES') {
+   				$('#customTitle').slideDown();
+				$('#customTitle input').prop("disabled", false);
+   			} else {
+   				$('#customTitle').slideUp();
+   				$('#customTitle input').prop("disabled", true);
+   			}
+   		});
+
+
+
+   		//Show/Hide Form Elements onchange of Report Type
+   		$('#reportType').change(function() {
+   			if(this.value == '0') {
+   				
+   				//Hide all Form Inputs
+   				$('.hideForm').slideUp();
+
+   			} else {
+
+   				//Hide all Form Inputs
+   				$('.hideForm').slideUp();
+
+   				if($('input[type=radio][name=customTitleCheck]').val() == "YES") {
+   					$('#customTitle input').prop("disabled", false);
+   				}
+
+   				//Disable all inputs
+   				$('.genRep input').prop("disabled", true);
+   				$('.genRep select').prop("disabled", true);
+
+   				//Enable CoverPage + Report Type Options
+   				$('.genRep #reportType').prop("disabled", false);
+   				$('.genRep input[name=optionalCoverPage]').prop("disabled", false);
+   				$('.genRep input[name=customTitleCheck]').prop("disabled", false);
+   				$('.reportName input').prop("disabled", false);
+   				$('.formSubmitCont input').prop("disabled", false);
+
+   				//Enable + Display required elements
+   				var arrayList = allArray[this.value];
+
+   				
+
+   				for(var i=0; i < arrayList.length; i++) {
+   					//Enable all associated
+   					$(indexInput[arrayList[i]] + ' input').prop("disabled", false);
+   					$(indexInput[arrayList[i]] + ' select').prop("disabled", false);
+   					$(indexInput[arrayList[i]] + ' textarea').prop("disabled", false);
+   					$(indexInput[arrayList[i]]).slideDown();
+   				}
+   				
+
+
+   				//Show the Generate Button
+   				$('.formSubmitCont').slideDown();
+   			}
+   		});
+
+   	</script>
   </body>
 </html>
